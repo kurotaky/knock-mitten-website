f527decf2bdd4dae6b0b7795afde6f277e9d60dd
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2019-05-04 00:41:51 +0900

    p5jsを使ってみる

--- static/app.js
+++ static/app.js
@@ -50,9 +50,53 @@ const setNotifications = (characteristic) => {
     if (text) {
       text.innerHTML = json.pulse
       counter.innerHTML = json.counter
+      updateColor() // 色を変更
     }
   })
 
   // Notifications開始
   characteristic.startNotifications()
+}
+
+//
+// p5js
+//
+
+// for red, green, and blue color values
+let r, g, b;
+
+function setup() {
+  var canvas = createCanvas(1000, 400);
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+  background(255, 0, 200);
+  // Pick colors randomly
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
+
+function draw() {
+  background(255);
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(360, 200, 100, 100);
+  ellipse(30, 30, 50, 50);
+  ellipse(30, 230, 30, 30);
+  ellipse(160, 100, 50, 50);
+  line(160, 200, 300, 400)
+  line(280, 100, 500, 150)
+  ellipse(560, 100, 30, 30);
+  ellipse(360, 50, 20, 20);
+  triangle(230, 275, 258, 220, 286, 275);
+}
+
+// When the user clicks the mouse
+function updateColor() {
+  // Pick new random color values
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
